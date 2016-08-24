811d6fe82dd701d236df6130d252c85f9bfcb68b
Karolis Vyčius <karolis@trafi.com>
2016-08-24 12:11:41 +0200

    Add schedules

--- app/src/main/java/com/nmakademija/nmaakademija/api/NMAService.java
+++ app/src/main/java/com/nmakademija/nmaakademija/api/NMAService.java
@@ -1,6 +1,6 @@
 package com.nmakademija.nmaakademija.api;
 
-import com.nmakademija.nmaakademija.entity.Event;
+import com.nmakademija.nmaakademija.entity.ScheduleEvent;
 import com.nmakademija.nmaakademija.entity.TimeTillSession;
 
 import java.util.List;
@@ -11,7 +11,7 @@ import retrofit2.http.GET;
 public interface NMAService {
 
     @GET("api/events")
-    Call<List<Event>> getEvents();
+    Call<List<ScheduleEvent>> getEvents();
 
     @GET("api/tts")
     Call<TimeTillSession> getTimeTillSession();
