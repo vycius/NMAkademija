006de9e29a4dc4090a23ad847aefa2a6493fb59f
gediminasel <gediminasel@gmail.com>
2016-08-24 17:40:02 +0300

    Delete default comments

--- app/src/main/java/com/nmakademija/nmaakademija/service/NMAFirebaseInstanceIDService.java
+++ app/src/main/java/com/nmakademija/nmaakademija/service/NMAFirebaseInstanceIDService.java
@@ -5,9 +5,6 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-/**
- * Created by gaust on 8/23/2016.
- */
 public class NMAFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
     private static final String TAG = "FirebaseIIDService";
