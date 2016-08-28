3d6b239eaf67e433fbb8f6eeeed5e8670574538d
gediminasel <gediminasel@gmail.com>
2016-08-28 15:58:18 +0300

    Update UserRoles.java

--- app/src/main/java/com/nmakademija/nmaakademija/entity/UserRoles.java
+++ app/src/main/java/com/nmakademija/nmaakademija/entity/UserRoles.java
@@ -8,12 +8,16 @@ import com.google.gson.annotations.SerializedName;
 public enum UserRoles {
     @SerializedName("teacher")
     TEACHER,
+    
     @SerializedName("member")
     MEMBER,
+    
     @SerializedName("guest")
     GUEST,
+    
     @SerializedName("supervisor")
     SUPERVISOR,
+    
     @SerializedName("administrator")
     ADMINISTRATOR,
 }
