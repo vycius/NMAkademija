720942d700ba2d1476fd583629d06c04fa12421b
gediminasel <gediminasel@gmail.com>
2016-09-05 16:13:32 +0300

    deleted unused var

--- app/src/main/java/com/nmakademija/nmaakademija/fragment/SettingsFragment.java
+++ app/src/main/java/com/nmakademija/nmaakademija/fragment/SettingsFragment.java
@@ -1,19 +1,13 @@
 package com.nmakademija.nmaakademija.fragment;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.nmakademija.nmaakademija.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
-    SharedPreferences sharedPreferences;
-
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.preferences);
-
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext().getApplicationContext());
     }
 }
