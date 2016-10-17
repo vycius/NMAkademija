4426b7aee93bc92d3f18a7bc7cea3773cc01ee99
gediminasel <gediminasel@gmail.com>
2016-10-17 18:51:19 +0300

    fix

--- app/src/main/java/com/nmakademija/nmaakademija/utils/Error.java
+++ app/src/main/java/com/nmakademija/nmaakademija/utils/Error.java
@@ -1,14 +1,11 @@
 package com.nmakademija.nmaakademija.utils;
 
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.TextView;
 
-import com.nmakademija.nmaakademija.BaseActivity;
 import com.nmakademija.nmaakademija.R;
 
 public class Error {
@@ -18,13 +15,6 @@ public class Error {
             Snackbar snackbar = Snackbar.make(view, R.string.request_failed, Snackbar.LENGTH_INDEFINITE);
             TextView tv = (TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
             tv.setTextColor(Color.WHITE);
-            Context viewContext = view.getContext();
-            while (viewContext instanceof ContextWrapper) {
-                if (viewContext instanceof BaseActivity) {
-                    break;
-                }
-                viewContext = ((ContextWrapper) viewContext).getBaseContext();
-            }
             snackbar.setAction(R.string.retry, listener);
             snackbar.show();
         }
