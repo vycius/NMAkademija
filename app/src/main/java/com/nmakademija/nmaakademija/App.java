5631c31613af86836c3a4d6b61747c067cc58d18
Karolis Vycius <k.vycius@gmail.com>
2017-01-05 23:46:04 +0200

    Install Leakcanary for leak detection

--- app/src/main/java/com/nmakademija/nmaakademija/App.java
+++ app/src/main/java/com/nmakademija/nmaakademija/App.java
@@ -3,12 +3,19 @@ package com.nmakademija.nmaakademija;
 import android.app.Application;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.leakcanary.LeakCanary;
 
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
