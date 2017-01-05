b3071c382f50ff62100c6705ecfeaae9d1ab67bd
Karolis Vycius <k.vycius@gmail.com>
2017-01-06 00:19:28 +0200

    Remove unused NMAServices

--- app/src/main/java/com/nmakademija/nmaakademija/api/NMAService.java
+++ app/src/main/java/com/nmakademija/nmaakademija/api/NMAService.java
@@ -1,10 +1,7 @@
 package com.nmakademija.nmaakademija.api;
 
 import com.nmakademija.nmaakademija.entity.Article;
-import com.nmakademija.nmaakademija.entity.ScheduleEvent;
-import com.nmakademija.nmaakademija.entity.Section;
 import com.nmakademija.nmaakademija.entity.TimeUntilSession;
-import com.nmakademija.nmaakademija.entity.Academic;
 
 import java.util.List;
 
@@ -13,18 +10,10 @@ import retrofit2.http.GET;
 
 public interface NMAService {
 
-    @GET("api/events/")
-    Call<List<ScheduleEvent>> getEvents();
-
     @GET("api/tts/")
     Call<TimeUntilSession> getTimeTillSession();
-
-    @GET("api/users/")
-    Call<List<Academic>> getUsers();
 
     @GET("api/news/")
     Call<List<Article>> getArticles();
 
-    @GET("api/sections/")
-    Call<List<Section>> getSections();
 }
