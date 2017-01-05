bad66f43ab4ee65b6f5918561618dc257a151564
Karolis Vycius <k.vycius@gmail.com>
2017-01-05 23:41:39 +0200

    Enable persistance for Firebase

---
+++ app/src/main/java/com/nmakademija/nmaakademija/App.java
@@ -0,0 +1,15 @@
+package com.nmakademija.nmaakademija;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
