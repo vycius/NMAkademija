d23bf06645cddce746a4962d9c70918a73068de1
gediminasel <gediminasel@gmail.com>
2017-01-06 03:39:58 +0200

    change Activity to FragmentActivity

--- app/src/main/java/com/nmakademija/nmaakademija/StartActivity.java
+++ app/src/main/java/com/nmakademija/nmaakademija/StartActivity.java
@@ -1,25 +1,24 @@
 package com.nmakademija.nmaakademija;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.nmakademija.nmaakademija.utils.AppEvent;
 import com.nmakademija.nmaakademija.utils.NMAPreferences;
 
-public class StartActivity extends Activity {
+public class StartActivity extends FragmentActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
         boolean notificationsEnabled = NMAPreferences.getNotifications(this);
 
         AppEvent.getInstance(this).setNotificationEnabledUserProperty(notificationsEnabled);
-
         startActivity(new Intent(this, SignInActivity.class));
         finish();
     }
