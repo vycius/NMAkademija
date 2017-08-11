1ac3cb5da28dbc97e4bcb63562e7cb2b33b457cd
gediminasel <gediminasel@gmail.com>
2017-08-11 22:48:32 +0300

    save all user data

--- app/src/main/java/com/nmakademija/nmaakademija/EditProfileActivity.java
+++ app/src/main/java/com/nmakademija/nmaakademija/EditProfileActivity.java
@@ -1,19 +1,21 @@
 package com.nmakademija.nmaakademija;
 
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,6 +24,7 @@ import com.nmakademija.nmaakademija.api.listener.AcademicUpdatedListener;
 import com.nmakademija.nmaakademija.api.listener.AcademicsLoadedListener;
 import com.nmakademija.nmaakademija.entity.Academic;
 import com.nmakademija.nmaakademija.utils.AppEvent;
+import com.nmakademija.nmaakademija.utils.Error;
 
 import java.util.ArrayList;
 
@@ -29,8 +32,10 @@ public class EditProfileActivity extends BaseActivity implements AcademicsLoaded
 
     private Academic academic;
     private String academicEmail;
-   private EditText phoneView;
-
+    private EditText phoneView;
+    private EditText emailView;
+    private EditText bioView;
+    private EditText roomView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,14 +57,20 @@ public class EditProfileActivity extends BaseActivity implements AcademicsLoaded
 
         academicEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
         FirebaseRealtimeApi.getAcademicByEmail(this, academicEmail);
+        Button saveButton = (Button) findViewById(R.id.save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveUser();
+            }
+        });
     }
 
 
-    // No. This should return exaclty one academic instead of array
     @Override
     public void onAcademicsLoaded(ArrayList<Academic> academics) {
         if (academics.size() != 1) {
-            onLoadingFailed();
+            onLoadFailed();
         } else {
             academic = academics.get(0);
 
@@ -69,45 +80,57 @@ public class EditProfileActivity extends BaseActivity implements AcademicsLoaded
             TextView nameView = (TextView) findViewById(R.id.name_edit);
             nameView.setText(academic.getName());
 
-            EditText emailView = (EditText) findViewById(R.id.email_edit);
+            emailView = (EditText) findViewById(R.id.email_edit);
             emailView.setText(academic.getPublicEmail());
 
             phoneView = (EditText) findViewById(R.id.phone_edit);
             phoneView.setText(academic.getPhone());
 
-            EditText bioView = (EditText) findViewById(R.id.bio_edit);
+            bioView = (EditText) findViewById(R.id.bio_edit);
             bioView.setText(academic.getBio());
 
-            EditText roomView = (EditText) findViewById(R.id.room_edit);
+            roomView = (EditText) findViewById(R.id.room_edit);
             roomView.setText(academic.getRoom());
         }
     }
 
-    // Do not set listener via XML.
-    // Yup it's missing user
-    public void saveUser(View view) {
+    public void saveUser() {
         academic.setPhone(phoneView.getText().toString());
+        academic.setBio(bioView.getText().toString());
+        academic.setPublicEmail(emailView.getText().toString());
+        academic.setRoom(roomView.getText().toString());
 
         FirebaseRealtimeApi.updateAcademic(academic, new AcademicUpdatedListener() {
             @Override
             public void onAcademicUpdated(@NonNull Academic academic) {
-                Toast.makeText(EditProfileActivity.this, "Yup it works", Toast.LENGTH_SHORT).show();
+                Snackbar snackbar = Snackbar.make(findViewById(R.id.content), R.string.saved, Snackbar.LENGTH_SHORT);
+                TextView tv = (TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+                tv.setTextColor(Color.WHITE);
+                snackbar.show();
             }
 
             @Override
             public void onAcademicUpdateFailed(Exception exception) {
-                Toast.makeText(EditProfileActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
-
+                onSaveFailed();
             }
         });
     }
 
     @Override
     public void onAcademicsLoadingFailed(Exception exception) {
-        onLoadingFailed();
+        onLoadFailed();
     }
 
-    private void onLoadingFailed() {
-        finish();
+    private void onLoadFailed() {
+        Error.loadProfile(findViewById(R.id.content));
+    }
+
+    private void onSaveFailed() {
+        Error.saveData(findViewById(R.id.content), new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveUser();
+            }
+        });
     }
 }
--- app/src/main/java/com/nmakademija/nmaakademija/utils/Error.java
+++ app/src/main/java/com/nmakademija/nmaakademija/utils/Error.java
@@ -12,10 +12,29 @@ public class Error {
 
     public static void getData(@Nullable View view, View.OnClickListener listener) {
         if (view != null) {
-            Snackbar snackbar = Snackbar.make(view, R.string.request_failed, Snackbar.LENGTH_INDEFINITE);
+            Snackbar snackbar = Snackbar.make(view, R.string.get_request_failed, Snackbar.LENGTH_INDEFINITE);
             TextView tv = (TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
             tv.setTextColor(Color.WHITE);
             snackbar.setAction(R.string.retry, listener);
+            snackbar.show();
+        }
+    }
+
+    public static void saveData(@Nullable View view, View.OnClickListener listener) {
+        if (view != null) {
+            Snackbar snackbar = Snackbar.make(view, R.string.save_request_failed, Snackbar.LENGTH_INDEFINITE);
+            TextView tv = (TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+            tv.setTextColor(Color.WHITE);
+            snackbar.setAction(R.string.retry, listener);
+            snackbar.show();
+        }
+    }
+
+    public static void loadProfile(@Nullable View view) {
+        if (view != null) {
+            Snackbar snackbar = Snackbar.make(view, R.string.profile_request_failed, Snackbar.LENGTH_INDEFINITE);
+            TextView tv = (TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+            tv.setTextColor(Color.WHITE);
             snackbar.show();
         }
     }
