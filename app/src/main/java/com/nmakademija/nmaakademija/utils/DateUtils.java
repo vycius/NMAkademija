6510c533c6861f606ea779a9c7cdf301563831fb
gediminasel <gediminasel@gmail.com>
2017-08-15 20:04:26 +0300

    use ThreadLocal

--- app/src/main/java/com/nmakademija/nmaakademija/utils/DateUtils.java
+++ app/src/main/java/com/nmakademija/nmaakademija/utils/DateUtils.java
@@ -14,21 +14,29 @@ public class DateUtils {
     private static final String DATE_FORMAT_PATTERN_TZ = "yyyy-MM-dd'T'HH:mm:ss'Z'";
     private static final String TIME_FORMAT_PATTERN = "HH:mm";
 
-    private static final SimpleDateFormat TIME_FORMAT =
-            new SimpleDateFormat(TIME_FORMAT_PATTERN, Locale.US);
+    private static final ThreadLocal<SimpleDateFormat> TIME_FORMAT = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat(TIME_FORMAT_PATTERN, Locale.US);
+        }
+    };
 
-    private static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT_PATTERN_TZ,
-            Locale.US);
+    private static final ThreadLocal<SimpleDateFormat> DATE_FORMAT = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat(DATE_FORMAT_PATTERN_TZ, Locale.US);
+        }
+    };
 
     static {
-        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
-        TIME_FORMAT.setTimeZone(TimeZone.getDefault());
+        DATE_FORMAT.get().setTimeZone(TimeZone.getTimeZone("UTC"));
+        TIME_FORMAT.get().setTimeZone(TimeZone.getDefault());
     }
 
     @Nullable
     public static Date parseTZDate(String pattern) {
         try {
-            return DATE_FORMAT.parse(pattern);
+            return DATE_FORMAT.get().parse(pattern);
         } catch (ParseException e) {
             e.printStackTrace();
             return null;
@@ -41,7 +49,7 @@ public class DateUtils {
             return null;
         }
 
-        return TIME_FORMAT.format(date);
+        return TIME_FORMAT.get().format(date);
     }
 
 
