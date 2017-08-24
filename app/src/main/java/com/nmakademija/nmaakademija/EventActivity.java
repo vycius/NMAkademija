cf0b8ab80898dd3e97326ea56777463e94255e1f
Gustas <gaustas@gmail.com>
2017-08-24 11:23:51 +0300

    Started EventActivity

--- app/src/main/java/com/nmakademija/nmaakademija/EventActivity.java
+++ app/src/main/java/com/nmakademija/nmaakademija/EventActivity.java
@@ -1,5 +1,81 @@
 package com.nmakademija.nmaakademija;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.nmakademija.nmaakademija.adapter.FileBrowserAdapter;
+import com.nmakademija.nmaakademija.entity.FileItem;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 public class EventActivity extends BaseActivity {
     public static final String EXTRA_EVENT = "event";
+    private List<FileItem> files;
+    private RecyclerView recyclerView;
+    private FileBrowserAdapter fileBrowserAdapter;
+
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    private ImageView imageView;
+    private Uri imgURI;
+
+    public static final String FB_STORAGE_PATH = "image/";
+    public static final String FB_DATABASE_PATH = "image";
+    public static  final int REQUEST_CODE = 1234;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference(FB_DATABASE_PATH);
+
+        imageView = (imageView) findViewById();
+
+        recyclerView = findViewById(R.id.recycler);
+        fileBrowserAdapter = new FileBrowserAdapter(files);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(fileBrowserAdapter);
+    }
+
+        public void btnBrowse_Click(View v){
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select image"), REQUEST_CODE);
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null)
+            imgURI = data.getData();
+
+        try{
+            Bitmap bm = MediaStore.Images.Media.getBitmap(getContentResolver(), imgURI);
+            ImageView.setImageBitmap(bm);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
