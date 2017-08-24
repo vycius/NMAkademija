59007c27e3999bbf93f744242fec149c74c9f359
NeringaLevinskaite <neringa.levinskaite@gmail.com>
2017-08-24 10:41:06 +0300

    Add article create activity

---
+++ app/src/main/java/com/nmakademija/nmaakademija/ArticleCreateActivity.java
@@ -0,0 +1,18 @@
+package com.nmakademija.nmaakademija;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.nmakademija.nmaakademija.BaseActivity;
+
+/**
+ * Created by wallflower on 17.8.23.
+ */
+
+public class ArticleCreateActivity extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
